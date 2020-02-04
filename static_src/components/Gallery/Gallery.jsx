4d12a8db2ce378724f2d3c05f819f529141e9e6b
DmitryKeymakh <keel87@rambler.ru>
2020-02-05 01:32:24 +0300

    Переработал галерею. Нужны еще правки

--- static_src/components/Gallery/Gallery.jsx
+++ static_src/components/Gallery/Gallery.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
-import Carousel from './Carousel'
+import PropTypes from 'prop-types';
+import Swiper from 'swiper';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
+import 'swiper/css/swiper.css';
 import './_gallery.scss';
+
 
 export default class Gallery extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            // url: 'https://raw.githubusercontent.com/DmitryKeymakh/front/master/api/moto-images.json',
-            url: 'https://raw.githubusercontent.com/DmitryKeymakh/front/master/api/images.json',
+            url: '',
             images: [],
-            // photoIndex: 0,
-            // isOpen: false,
+            zoomImages: [],
+            photoIndex: 0,
+            isOpen: false,
         };
     }
 
     componentDidMount() {
 
-        fetch(this.state.url)
+        new Swiper('.swiper-container', {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            slidesPerGroup: 3,
+            loop: true,
+            loopFillGroupWithBlank: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+
+        fetch(this.props.url)
             .then(response => {
                 if (response.status === 200) {
                     return response.json();
@@ -25,9 +46,49 @@ export default class Gallery extends React.Component {
                     console.log(response.status);
                 }
             })
-            .then(json_data => {
-                this.setState({images: json_data});
-                return <Carousel images={json_data.images}/>;
+            .then(result => {
+                this.setState({images: result.images});
+                for (let item of result.images) {
+                    this.state.zoomImages.push(item.src);
+                }
             });
+    }
+
+    render() {
+        const { images, zoomImages, photoIndex, isOpen } = this.state;
+
+        return (
+            <div className="swiper-container">
+                <div className="swiper-wrapper">
+                    {images.map((image) =>
+                        <div className="swiper-slide" key={image.id}>
+                            <img onClick={() => this.setState({isOpen: true, photoIndex: images.indexOf(image)})} className="swiper-image" src={image.src} alt="Gallery-image"/>
+                        </div>
+                    )}
+                </div>
+                <div className="swiper-pagination"></div>
+                <div className="swiper-button-next"></div>
+                <div className="swiper-button-prev"></div>
+
+                {isOpen && (
+                    <Lightbox
+                        mainSrc={zoomImages[photoIndex]}
+                        nextSrc={zoomImages[(photoIndex + 1) % zoomImages.length]}
+                        prevSrc={zoomImages[(photoIndex + zoomImages.length - 1) % zoomImages.length]}
+                        onCloseRequest={() => this.setState({ isOpen: false })}
+                        onMovePrevRequest={() =>
+                            this.setState({
+                                photoIndex: (photoIndex + zoomImages.length - 1) % zoomImages.length,
+                            })
+                        }
+                        onMoveNextRequest={() =>
+                            this.setState({
+                                photoIndex: (photoIndex + 1) % zoomImages.length,
+                            })
+                        }
+                    />
+                )}
+            </div>
+        )
     }
 }
--- static_src/components/Layout.jsx
+++ static_src/components/Layout.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import Carousel from './Gallery/Carousel';
+import Gallery from './Gallery/Gallery';
 import '../styles/main.scss';
 
 export default class Layout extends React.Component {
 
     render() {
         return (
-            // <Carousel url={'https://raw.githubusercontent.com/DmitryKeymakh/front/master/api/moto-images.json'}/>
-            <Carousel url={'https://raw.githubusercontent.com/DmitryKeymakh/front/master/api/images.json'}/>
+            <Gallery url={'https://raw.githubusercontent.com/DmitryKeymakh/front/master/api/images.json'}/>
         )
     }
 }
