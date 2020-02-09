743e759bbff0b0c1699c964b68b5358cc6e0195a
DmitryKeymakh <keel87@rambler.ru>
2020-02-09 23:58:07 +0300

    тест

--- static_src/components/Forms/SignInForm.jsx
+++ static_src/components/Forms/SignInForm.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import './_forms.scss';
+import React, { useState } from 'react';
+import 'babel-polyfill';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import './_forms.scss';
 
+
+const PasswordShowHide = ({ field, form }) => {
+    const [showHidePassword, changeShowHidePassword] = useState(false);
+    const hasError = form.touched[field.name] && form.errors[field.name];
+
+    return (
+        <div className="input-container">
+            <i
+                className={hasError ? "icon-error icon" : "fa fa-key icon"}
+                onClick={() => changeShowHidePassword(!showHidePassword)}
+            >
+                i
+            </i>
+            <input
+                type={showHidePassword ? "text" : "password"}
+                {...field}
+                className={hasError ? "input-error input-field" : "input-field"}
+                placeholder="Password"
+            />
+        </div>
+    );
+};
 
 const SignInSchema = Yup.object({
+    email: Yup.string()
+        .email('Неправильный имейл')
+        .required('Введите имейл'),
+        // .test(
+        //     'email-check',
+        //     'Неверный имейл',
+        //     async () => (await fetch('https://raw.githubusercontent.com/DmitryKeymakh/front/master/api/check-email.json')).email,
+        // ),
     password: Yup.string()
         .required('Введите пароль')
         .min(6, 'Минимальная длина: 6 символов')
         .max(20, 'Максимальная длина: 20 символов')
-        .matches(/[a-zA-Z0-9]\w/, 'Используйте латинские буквы, цифры и знак нижнего подчеркивания'),
-    email: Yup.string()
-        .email('Неправильный имейл')
-        .required('Введите имейл'),
+        .matches(/[a-zA-Z0-9]\w/, 'Используйте латинские буквы, цифры и знак нижнего подчеркивания')
+        .test(
+            'password-check',
+            'Неверный пароль',
+            // async () => (await fetch('https://raw.githubusercontent.com/DmitryKeymakh/front/master/api/check-password.json')).statusText === 'OK',
+            async (value) => (await fetch('/is-jimmy/' + value)).responseText === 'true',
+        ),
 });
 
 export default function SignInForm() {
@@ -29,15 +63,6 @@ export default function SignInForm() {
         >
             {({ errors, touched }) => (
                 <Form>
-                    <label htmlFor="password">Пароль</label>
-                    <Field
-                        className={errors.password && touched.password ? ('text-input error') : ('text-input')}
-                        name="password"
-                        type="password" />
-                    <ErrorMessage
-                        component="div"
-                        className="input-feedback"
-                        name="password" />
                     <label htmlFor="email">Email Address</label>
                     <Field
                         className={errors.email && touched.email ? ('text-input error') : ('text-input')}
@@ -47,6 +72,17 @@ export default function SignInForm() {
                         component="div"
                         className="input-feedback"
                         name="email" />
+                    <label htmlFor="password">Пароль</label>
+                    <Field
+                        className={errors.password && touched.password ? ('text-input error') : ('text-input')}
+                        name="password"
+                        type="password"
+                        // component={PasswordShowHide}
+                    />
+                    <ErrorMessage
+                        component="div"
+                        className="input-feedback"
+                        name="password" />
                     <button type="reset">Reset</button>
                     <button type="submit">Submit</button>
                 </Form>
