92145176a251a88ff99fc5750caa1762ada1a1d3
keimahdv <keimahdv@mosmetod.ru>
2020-02-11 13:57:36 +0300

    Переместил Menu и DropdownProfile в Header (каталоги)

---
+++ static_src/components/Header/DropdownProfile/DropdownProfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './_dropdown-profile.scss';
+
+
+export default class DropdownProfile extends React.Component {
+
+    render() {
+        return (
+            <div className="dropdown-profile">
+
+            </div>
+        )
+    }
+}
---
+++ static_src/components/Header/Menu/Menu.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './_menu.scss';
+
+
+export default class Menu extends React.Component {
+
+    render() {
+        return (
+            <div className="menu">
+                <Link className="menu-item" to="/">главная</Link>
+                <Link className="menu-item" to="/second">вторая</Link>
+                <Link className="menu-item" to="/third">третья</Link>
+                <Link className="menu-item" to="/sign-in">авторизация</Link>
+                <Link className="menu-item" to="/sign-up">регистрация</Link>
+            </div>
+        )
+    }
+}
