25920c7c06960b7b09e26c10d4afcdc59091d1a6
keimahdv <keimahdv@mosmetod.ru>
2020-02-18 17:39:21 +0300

    Доработал webpack.config.js Добавил тестовую анимацию на библиотеке react-springs

--- static_src/components/Header/DropdownProfile/DropdownProfile.jsx
+++ static_src/components/Header/DropdownProfile/DropdownProfile.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import './_dropdown-profile.scss';
+import React from 'react';
+import App from './index'
 
 
 export default class DropdownProfile extends React.Component {
@@ -7,7 +8,7 @@ export default class DropdownProfile extends React.Component {
     render() {
         return (
             <div className="dropdown-profile">
-
+                <App />
             </div>
         )
     }
