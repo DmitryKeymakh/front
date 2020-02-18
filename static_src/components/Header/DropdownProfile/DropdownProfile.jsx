0d7902a5c20d666eac58604bedfa46a2daa431c4
DmitryKeymakh <keel87@rambler.ru>
2020-02-19 00:23:55 +0300

    Добавил анимацию на выпадающее меню в компоненте DropdownProfile.jsx

--- static_src/components/Header/DropdownProfile/DropdownProfile.jsx
+++ static_src/components/Header/DropdownProfile/DropdownProfile.jsx
@@ -1,14 +1,36 @@
 import './_dropdown-profile.scss';
 import React from 'react';
-import App from './index'
+import { Transition, animated } from 'react-spring/renderprops';
 
 
 export default class DropdownProfile extends React.Component {
+    state = { show: false };
+    toggle = () => this.setState(state => ({ show: !state.show }));
 
     render() {
+        console.log(this.state.show);
         return (
             <div className="dropdown-profile">
-                <App />
+                <div className="dropdown-profile-button" onClick={this.toggle}>Войти</div>
+                <Transition
+                    // native
+                    items={this.state.show}
+                    trail={10}
+                    from={{ opacity: 0, transform: 'scale(0.9, 1) translateY(0px)',}}
+                    enter={[{ opacity: 1, transform: 'scale(1, 1) translateY(10px)' }]}
+                    leave={{ opacity: 0 }}
+                >
+
+                    {show =>
+                        show && (props =>
+                            <animated.div
+                                className="dropdown-profile-menu"
+                                style={props}
+                            >
+                                {/*hello*/}
+                            </animated.div>)
+                    }
+                </Transition>
             </div>
         )
     }
